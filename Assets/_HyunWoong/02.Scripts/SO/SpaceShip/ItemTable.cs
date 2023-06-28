154e3e5eb7050466098fa5699c346e3e21ec8506
김준호 <98887373+Kimjunho06@users.noreply.github.com>
2023-06-28 18:13:17 +0900

    init

--- Assets/_HyunWoong/02.Scripts/SO/SpaceShip/ItemTable.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(menuName ="SO/ItemTable")]
-public class ItemTable : ScriptableObject
-{
-    public List<ItemSO> DropItem = new List<ItemSO>();
-}
--- Assets/_Junho/02.Scripts/Combination/CombineRecipeSO.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu]
-public class CombineRecipeSO : ScriptableObject
-{
-    public ItemSO[] items;
-}
